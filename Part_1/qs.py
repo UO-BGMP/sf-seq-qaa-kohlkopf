dcf16f39d37d348e35bc4aa4b60b2a06d7871636
kohlkopf <kohlkinning@gmail.com>
2017-10-02 16:36:02 -0700

    Added original qscore code, part 1 and 2 updated

---
+++ Part_1/qs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.6
+
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+import argparse
+
+
+def get_lines(nthLine):
+    qualityLinesList = []
+    with open ("10_2G_both_S8_L008_R2_001.fastq") as fh:
+        NR = 0
+        for line in fh:
+            line = line.strip("\n")
+            NR += 1
+            if NR % 4 == nthLine:  #every 4th line starting at 0
+                qualityLinesList.append(line)
+    return qualityLinesList
+
+def convert_phred(letter):
+    return (ord(letter)-33)
+
+
+lines = get_lines(0)
+c = 0
+i = 0
+
+scores = np.zeros((101, len(lines)), dtype = float)
+
+for c in range(101):
+    i = 0
+    for line in lines:
+        scores[c][i] = convert_phred(line[c])  #convert cth char on quality line and add to qualityList
+        i+=1
+    c+=1
+
+for pos in range(101):
+    means = scores[pos].mean()
+
+print("BP Position", "\t", "Mean Score", "\t", "Variance", "\t", "StDev", "\t", "Median")
+for pos in range(101):
+    print(pos, "\t", scores[pos].mean(), "\t", scores[pos].var(), "\t", scores[pos].std(), "\t", np.median(scores[pos]))
